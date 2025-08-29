1897333f18788f73edc5a08240e95d2e4020957a
coolbulldj <matthewjcdt@gmail.com>
2025-08-29 14:06:56 +1000

    working buttons omg ts is coming along so goodly

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.getElementById("HomeButton").onclick = function() {
+    alert("Home button clicked!");
+};
+
+document.getElementById("ContactButton").onclick = function() {
+    alert("Contact button clicked!");
+};
+
+document.getElementById("DevTeamButton").onclick = function() {
+    alert("Development Team button clicked!");
+};
+
+document.getElementById("FrameworksButton").onclick = function() {
+    alert("Frameworks button clicked!");
+};
+
+document.getElementById("GamesButton").onclick = function() {
+    alert("Games button clicked!");
+};
+
+document.getElementById("SubmitContactButton").onclick = function() {
+    alert("Submit Contact button clicked!");
+};
