de6f899ac0f450cc17e52d8b968ce10c9a964452
coolbulldj <matthewjcdt@gmail.com>
2025-08-29 03:17:12 -0400

    buttons are so cool

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 
-document.getElementById("HomeButton").onclick = function() {
-    alert("Home button clicked!");
-};
+function HomeButtonClick() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
 
-document.getElementById("ContactButton").onclick = function() {
-    alert("Contact button clicked!");
-};
+function ContactButtonClick() {
+    document.getElementById("ContactSection").scrollIntoView({ behavior: 'smooth' });
+}
 
-document.getElementById("DevTeamButton").onclick = function() {
-    alert("Development Team button clicked!");
-};
+function DevTeamButtonClick() {
+    document.getElementById("TeamSection").scrollIntoView({ behavior: 'smooth' });
+}
 
-document.getElementById("FrameworksButton").onclick = function() {
-    alert("Frameworks button clicked!");
-};
+function FrameworksButtonClick() {
+    document.getElementById("FrameworkSection").scrollIntoView({ behavior: 'smooth' });
+}
 
-document.getElementById("GamesButton").onclick = function() {
-    alert("Games button clicked!");
-};
+function GamesButtonClick() {
+    document.getElementById("GamesSection").scrollIntoView({ behavior: 'smooth' });
+}
+
+//Social Media redirects
+function TwitterRedirect() {
+    window.open("https://twitter.com/RedPaintGames", "_blank");
+}
+
+function YouTubeRedirect() {
+    window.open("InstagramIcon", "_blank");
+}
+
+function DiscordRedirect() {
+    window.open("https://discord.gg/wHB59j9Z3H", "_blank");
+}
+
+function InstagramRedirect() {
+    window.open("https://www.instagram.com/redpaintgames/", "_blank");
+}
+
+// Assign the functions to the buttons
+
+document.getElementById("HomeButton").onclick = HomeButtonClick;
+
+document.getElementById("ContactButton").onclick = ContactButtonClick;
+
+document.getElementById("DevTeamButton").onclick = DevTeamButtonClick;
+
+document.getElementById("FrameworksButton").onclick = FrameworksButtonClick;
+
+document.getElementById("GamesButton").onclick = GamesButtonClick;
 
 document.getElementById("SubmitContactButton").onclick = function() {
+    window.location.href = "https://example.com";
     alert("Submit Contact button clicked!");
 };
+
+document.getElementById("TwitterIcon").onclick = TwitterRedirect;
+document.getElementById("YouTubeIcon").onclick = YouTubeRedirect;
+document.getElementById("DiscordIcon").onclick = DiscordRedirect;
+document.getElementById("InstagramIcon").onclick = InstagramRedirect;
