ca2a7bbada8df4dbd65d7a7cbeadf8d2e992d2d8
coolbulldj <matthewjcdt@gmail.com>
2025-08-30 21:41:43 -0400

    backend server setup, post http requests setup.

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
 const frontend_path = path.join(__dirname, "..", "frontend");
 
-app.use(express.static(frontend_path))
+app.use(
+    express.json(),
+    express.static(frontend_path)
+)
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(frontend_path, 'home.html'))
-});
+    res.sendFile(path.join(frontend_path, 'home.html'));
+})
 
-app.get('/api/contact', (req, res) => {
-    res.send([1, 2, 3]);
-});
+app.post('/api/contact', (req, res) => {
+    console.log(req.body)
+    
+    //const transporter = nodemailer.createTransport()
 
-app.get('/api/contact/:', (req, res) => {
-    res.send(req.query);
+    return res.sendStatus(200);
 })
 
 //Port
--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,17 @@
 const home_url = "/home.html"
 const frameworks_url = "/framework.html"
 
+const contact_api_url = "/api/contact"
+const purchase_api_url = ""
+
+
+//Elements
+const message_input = document.getElementById('MessageInput')
+const name_input = document.getElementById('NameInput')
+const email_input = document.getElementById('EmailInput')
+const discord_input = document.getElementById('DiscordInput')
+const phone_number_input = document.getElementById('PhoneInput')
+
 function HomeButtonClick() {
     if (window.location.pathname !== home_url) {
         window.location.replace(home_url)
@@ -27,6 +38,26 @@ function FrameworksButtonClick() {
 
 function GamesButtonClick() {
     document.getElementById("GamesSection").scrollIntoView({ behavior: 'smooth' });
+}
+
+function SubmitButtonClick() {
+    alert('your current message is ' + message_input.value)
+    
+    const url = window.location.origin + contact_api_url
+    alert(url)
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            "name" : name_input.value,
+            "email" : email_input.value,
+            "phone" : phone_number_input.value,
+            "discord" : discord_input.value,
+            "message" : message_input.value
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
 }
 
 //Social Media redirects
@@ -58,10 +89,7 @@ document.getElementById("FrameworksButton").onclick = FrameworksButtonClick;
 
 document.getElementById("GamesButton").onclick = GamesButtonClick;
 
-document.getElementById("SubmitContactButton").onclick = function() {
-    window.location.href = "https://example.com";
-    alert("Submit Contact button clicked!");
-};
+document.getElementById("SubmitContactButton").onclick = SubmitButtonClick;
 
 document.getElementById("TwitterIcon").onclick = TwitterRedirect;
 document.getElementById("YouTubeIcon").onclick = YouTubeRedirect;
